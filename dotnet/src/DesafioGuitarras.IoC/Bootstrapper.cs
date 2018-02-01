a941fd30fdbd34a97c2e340e5cb4668b1ca8cc60
Henrique <henrique.d.bello@gmail.com>
2018-02-01 00:57:25 -0200

    Camada de Injeção de dependencia e Inversão de Controle completa

--- dotnet/src/DesafioGuitarras.IoC/Bootstrapper.cs
+++ dotnet/src/DesafioGuitarras.IoC/Bootstrapper.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DesafioGuitarras.Data.Context;
+using DesafioGuitarras.Data.Repositories;
+using DesafioGuitarras.Data.UoW;
+using DesafioGuitarras.Domain.Interfaces;
+using DesafioGuitarras.Domain.Interfaces.Repositories;
+using DesafioGuitarras.Domain.Interfaces.Services;
+using DesafioGuitarras.Domain.Services;
+using SimpleInjector;
 
 namespace DesafioGuitarras.IoC
 {
-    public class Bootstrapper
+    public static class Bootstrapper
     {
+        public static void RegisterContainer(Container container)
+        {
+            container.Register<IEletricGuitarRepository, EletricGuitarRepository>(Lifestyle.Scoped);
+            container.Register<IEletricGuitarService, EletricGuitarService>(Lifestyle.Scoped);
+            container.Register<IUnitOfWork, UnitOfWork>(Lifestyle.Scoped);
+
+            container.Register<EletricGuitarChallengeContext>(Lifestyle.Scoped);
+        }
     }
 }
